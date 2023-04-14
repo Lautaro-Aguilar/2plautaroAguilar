3241787f4be9e4ce09bf8fda2526474db916e349
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-04-13 21:33:25 -0300

    Clase 2, ejercicio mostrar nombre

---
+++ clase 1/js/funciones.js
@@ -0,0 +1,18 @@
+const texto = document.getElementById('inputTexto');
+
+// const inputEnviar = document.getElementById('inputEnviar');
+  
+// let texto = ''
+// inputTexto.addEventListener('keyup', (e) => {
+//   texto = (e.target.value)
+// })
+
+// inputEnviar.addEventListener('click', (e) => {
+//   e.preventDefault()
+//   console.log(texto)
+// })
+
+const mostrarValor = () => {
+  console.log('El valor ingresado es: ', 
+  texto.value)
+}

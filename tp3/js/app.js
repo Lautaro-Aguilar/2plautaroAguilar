087621a7168975217045054c6f045cf82cc748c9
Lautaro Aguilar <aguilarlautaroexequiel867@gmail.com>
2023-05-18 21:24:58 -0300

    update files

--- tp3/js/app.js
+++ tp3/js/app.js
@@ -1,43 +1,60 @@
-const especie1 = document.getElementById("especie1");
-const estado1 = document.getElementById("estado1");
-const nombre1 = document.getElementById("nombre1");
-const imagen1 = document.getElementById("imagen");
-const episodio1 = document.getElementById("episodios1");
-const cantEpisodios1 = document.getElementById("cantEpisodios1");
-const consultar = document.getElementById("consultar");
-const especie2 = document.getElementById("especie2");
-const estado2 = document.getElementById("estado2");
-const nombre2 = document.getElementById("nombre2");
-const imagen2 = document.getElementById("imagenTwo");
-const episodio2 = document.getElementById("episodios2");
-const cantEpisodios2 = document.getElementById("cantEpisodios2");
+const DEFAULT_CORDS = [1,1]
+var map = L.map('map', { center: DEFAULT_CORDS, zoom: 1});
 
-const ganador = document.getElementById("ganador");
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-let numero1 = document.getElementById("numero1");
-let numero2 = document.getElementById("numero2");
-let resultado = {};
+var marker = L.marker([-99999, -99999]);
 
-consultar.addEventListener('click', async () => {
-  const prueba = await fetch(`https://rickandmortyapi.com/api/character/${[numero2.value]}, ${[numero1.value]} `)
-  const pruebaJson = await prueba.json()
-  console.log('pj1: ', pruebaJson[0].name, 'pj2: ', pruebaJson[1].name)
+const botonConsultar = document.getElementById("consultar");
+const inputId = document.getElementById("numero");
+const mapa = document.getElementById('map')
+let latitudvar = ''
+let longitudvar = ''
+botonConsultar.addEventListener("click", async () => {
+  map.removeLayer(marker)
+  const nombre = document.getElementById("nombre");
+  const apellido = document.getElementById("apellido");
+  const codigo = document.getElementById("codigo");
+  const contenedor = document.getElementById('test')
+  const latitud = document.getElementById('latitud')
+  const longitud = document.getElementById('longitud')
+  const imagen = document.getElementById('imagen')
+  const genero = document.getElementById('genero')
+  
+  const response = await fetch("https://randomuser.me/api/");
+  const responseJson = await response.json();
+  const resultado = responseJson.results[0];
 
-  especie1.textContent = pruebaJson[0].species === 'Human' ? 'Humano' : 'Alien';
-  especie2.textContent = pruebaJson[1].species === 'Human' ? 'Humano' : 'Alien';
+  nombre.textContent = resultado.name.title + ' ' + resultado.name.first
+  apellido.textContent = resultado.name.last
 
-  nombre1.textContent = pruebaJson[0].name;
-  nombre2.textContent = pruebaJson[1].name;
+  codigo.textContent = resultado.id.name + ': ' + resultado.id.value
 
-  estado1.textContent = pruebaJson[0].status === 'Alive' ? 'Vivo' : 'Muerto';
-  estado2.textContent = pruebaJson[1].status === 'Alive' ? 'Vivo' : 'Muerto';
+  latitud.textContent = resultado.location.coordinates.latitude
+  longitud.textContent = resultado.location.coordinates.longitude
 
-  cantEpisodios1.textContent = pruebaJson[0].episode.length;
-  cantEpisodios2.textContent = pruebaJson[1].episode.length;
+  latitudvar = resultado.location.coordinates.latitude
+  longitudvar = resultado.location.coordinates.longitude
+  imagen.src = resultado.picture.medium
 
-
-  imagen1.src = pruebaJson[0].image;
-  imagen2.src = pruebaJson[1].image;
-  ganador.textContent = pruebaJson[0].episode.length > pruebaJson[1].episode.length ? 'Ganador: ' + pruebaJson[0].name : pruebaJson[0].episode.length < pruebaJson[1].episode.length ? 'Ganador: ' + pruebaJson[1].name : pruebaJson[0].episode.length === pruebaJson[1].episode.length ? 'Empate' : '';
-
-})
+  genero.textContent = resultado.gender
+  if (genero.textContent === 'male') {
+    contenedor.className = '';
+    contenedor.classList.add('textoAmarillo')
+  } else {
+    contenedor.className = ''
+    contenedor.classList.add('textoVerde')
+  }
+  var myIcon = L.icon({
+    iconUrl: 'https://static.vecteezy.com/system/resources/previews/009/267/042/original/location-icon-design-free-png.png',
+    iconSize: [25, 25],
+    iconAnchor: [30, 70],
+    popupAnchor: [-16, -75],
+  })
+  marker = L.marker([latitudvar, longitudvar], {icon: myIcon}).addTo(map);
+  marker.bindPopup(`<b>Acá vive: ${resultado.name.title + ' ' + resultado.name.first}!</b>`).openPopup();
+  map.panTo(new L.LatLng(latitudvar,longitudvar))
+});
